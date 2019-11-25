6cf94a5e017aabf498d59c47b7158d58af6be131
Isuru Fernando <isuruf@gmail.com>
2019-11-25 00:04:47 +0000

    Try autotools conda win

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ./configure --prefix=$PREFIX --with-tiff=$PREFIX --with-jpeg=$PREFIX
+[[ "$target_platform" == "win-64" ]] && patch_libtool
 make -j${CPU_COUNT}
 make check
 make install
