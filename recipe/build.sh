8393aa79a6c9cb07cfee75b63b41795ab4301e83
Mark Harfouche <mark.harfouche@gmail.com>
2020-01-01 20:03:54 -0500

    With libtiff

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,11 @@
 if [ ! -z ${LIBRARY_PREFIX+x} ]; then
     autoconf
     ./configure \
-      --prefix=$UNIX_LIBRARY_PREFIX
-      # --with-tiff=$UNIX_LIBRARY_PREFIX --with-jpeg=$UNIX_LIBRARY_PREFIX
+      --prefix=$UNIX_LIBRARY_PREFIX \
+      --with-tiff=$UNIX_LIBRARY_PREFIX
+      # We have trouble compiling with jpeg support on windows
+      # https://github.com/conda-forge/jpeg-feedstock/issues/19
+      # --with-jpeg=$UNIX_LIBRARY_PREFIX
 else
     ./configure --prefix=$PREFIX --with-tiff=$PREFIX --with-jpeg=$PREFIX
 fi
